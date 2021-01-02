098709aad6d2f4f89522f3f0da73c5cba30c47c1
Jidant Jha <72862678+JidantJha@users.noreply.github.com>
2021-01-02 16:21:50 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,26 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:10,
+        }
+        this.body = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world,this.body);
+    }
+
+    release(){
+        this.body.bodyA=null
+    }
+    display(){
+       if(this.body.bodyA){
+           var pointA = this.body.bodyA.position;
+           var pointB = this.pointB;
+           strokeWeight(1);
+           stroke("grey");
+           line(pointA.x,pointA.y,pointB.x,pointB.y);
+       }
+    }
+}
